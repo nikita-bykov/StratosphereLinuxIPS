561dd8036327eeb9b7d180b915d163aa89d8342b
alya <alyaggomaa@gmail.com>
2021-05-14 21:21:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# Installing slips dependencies
 
+# Installing slips dependencies
 echo "[+] Executing 'sudo apt-get update'"
 sudo apt-get update
 echo "[+] Executing 'sudo apt-get -y install curl git redis python3.7-minimal python3-redis python3-pip python3-watchdog nodejs npm'"
 sudo apt-get -y install curl git redis python3.7-minimal python3-redis python3-pip python3-watchdog nodejs npm
 echo "[+] Executing 'python3 -m pip install --upgrade pip'"
 python3 -m pip install --upgrade pip
-echo "[+] Executing 'pip3 install maxminddb colorama validators urllib3 numpy sklearn pandas certifi keras redis==3.4.1 pickle'"
-pip3 install maxminddb colorama validators urllib3 numpy sklearn pandas certifi keras redis==3.4.1 pickle
+echo "[+] Executing 'pip3 install maxminddb colorama validators urllib3 numpy sklearn pandas certifi keras redis==3.4.1 pickle zat pyod'"
+pip3 install maxminddb colorama validators urllib3 numpy sklearn pandas certifi keras redis==3.4.1 pickle zat pyod
 echo "[+] Executing 'sudo npm install blessed blessed-contrib redis async chalk strip-ansi clipboardy fs sorted-array-async'"
 sudo npm install blessed blessed-contrib redis async chalk strip-ansi clipboardy fs sorted-array-async
 
@@ -33,7 +33,7 @@ export PATH=$PATH:/usr/local/zeek/bin
 echo "[+] Adding /usr/local/zeek/bin to ~/.bashrc"
 echo "export PATH=$PATH:/usr/local/zeek/bin" >> ~/.bashrc
 
-# running slips for the first time
+# Running slips for the first time
 echo "[+] Executing 'redis-server --daemonize yes'"
 redis-server --daemonize yes
 
