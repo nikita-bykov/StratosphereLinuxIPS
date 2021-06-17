af3573b589465ce89110cf7df8d6e44af3d77d90
alya <alyaggomaa@gmail.com>
2021-06-17 18:51:21 +0200

    only load files that end with 'module'

--- slips.py
+++ slips.py
@@ -27,7 +27,7 @@ import shutil
 from datetime import datetime
 import socket
 import warnings
-from modules.UpdateManager.update_file_manager import UpdateFileManager
+from modules.update_manager.update_file_manager import UpdateFileManager
 import json
 import pkgutil
 import inspect
@@ -144,17 +144,24 @@ def load_modules(to_ignore):
             # indicates both absolute and relative imports will be attempted. 0 means only perform 
             # absolute imports. Positive values for level indicate the number of parent 
             # directories to search relative to the directory of the module calling __import__()."
-            module = importlib.import_module(module_name)
+            if module_name.endswith('module'):
+                module = importlib.import_module(module_name)
+                #todo document this
+                #todo edit module name in slips.conf and test b4 stopping branch
         except ImportError as e:
             print("Something wrong happened while importing the module {0}: {1}".format(module_name, e))
             continue
 
         # Walk through all members of currently imported modules.
-        for member_name, member_object in inspect.getmembers(module):
-            # Check if current member is a class.
-            if inspect.isclass(member_object):
-                if issubclass(member_object, Module) and member_object is not Module:
-                    plugins[member_object.name] = dict(obj=member_object, description=member_object.description)
+        try:
+            for member_name, member_object in inspect.getmembers(module):
+                # Check if current member is a class.
+                if inspect.isclass(member_object):
+                    if issubclass(member_object, Module) and member_object is not Module:
+                        plugins[member_object.name] = dict(obj=member_object, description=member_object.description)
+        except UnboundLocalError:
+            # not a module, skip
+            pass
 
     return plugins
 
